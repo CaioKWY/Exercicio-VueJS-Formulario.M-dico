7e2a0cc794473a9c9c1e9a19ebea64b9efe7772e
CaioKwy <caiokaway@gmail.com>
2023-05-20 23:09:07 -0300

    ajuste identação

--- script.js
+++ script.js
@@ -1,57 +1,50 @@
 const vm = new Vue({
-    el: "#app",
-    data: {
-        sexoP: "",
-        nascP: "",
-        nomeP: "",
-        planoP:"",
-        dataP:"",
-        pacientes: []
+  el: "#app",
+  data: {
+    sexoP: "",
+    nascP: "",
+    nomeP: "",
+    planoP: "",
+    dataP: "",
+    pacientes: [],
+  },
+  methods: {
+    cadastrar() {
+      this.pacientes.push({
+        nome: this.nomeP,
+        sexo: this.sexoP,
+        nascimento: this.nascP,
+        plano: this.planoP,
+      });
 
-
+      this.nomeP = "";
+      this.sexoP = "";
+      this.nascP = "";
+      this.planoP = "";
     },
-    methods:{
-        cadastrar(){
-            this.pacientes.push({
-                nome: this.nomeP,
-                sexo: this.sexoP,
-                nascimento: this.nascP,
-                plano: this.planoP
-            })
+  },
+  computed: {
+    lastP() {
+      let chave = this.pacientes.length - 1;
+      let txt = "";
 
-            this.nomeP = ""
-            this.sexoP = ""
-            this.nascP = ""
-            this.planoP = ""
+      txt += "Paciente: " + this.pacientes[chave].nome;
 
-        }
+      txt += ", " + this.pacientes[chave].nascimento;
+      txt += ", " + this.pacientes[chave].sexo;
+
+      return txt;
     },
-    computed:{
-        lastP(){
-            let chave = this.pacientes.length - 1 
-            let txt =""
 
-            txt += "Paciente: "+ this.pacientes[chave].nome
-           
-            txt += ", " + this.pacientes[chave].nascimento
-            txt += ", "+ this.pacientes[chave].sexo
+    planoB() {
+      return this.pacientes.filter((x1) => x1.plano == "Basico");
+    },
 
-            return txt
-        },
-
-        planoB(){
-            return this.pacientes.filter(x1 => x1.plano == "Basico")
-
-        },
-
-        planoPlus(){
-            return this.pacientes.filter(xyt => xyt.plano == "Plus")
-
-        },
-        planoC(){
-            return this.pacientes.filter(x1 => x1.plano == "Completo")
-
-        }
-    }
-
-})
+    planoPlus() {
+      return this.pacientes.filter((xyt) => xyt.plano == "Plus");
+    },
+    planoC() {
+      return this.pacientes.filter((x1) => x1.plano == "Completo");
+    },
+  },
+});
